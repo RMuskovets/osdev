d7207ca84cc543cb4052e3a202ef4bbe64bb7f78
RMuskovets <rmuskovets@gmail.com>
2019-03-25 09:39:13 +0200

    TCP packet structure

--- src/include/tcp.h
+++ src/include/tcp.h
@@ -3,7 +3,16 @@
 #include <system.h>
 
 typedef struct tcp_packet {
-
-}tcp_packet_t;
+	uint16_t sport;
+	uint16_t dport;
+	uint32_t sqnum;
+	uint32_t acnum;
+	uint8_t  doff_reserved_ns;
+	uint8_t  flags;
+	uint16_t winsz;
+	uint16_t checksum;
+	uint16_t urgent;
+	uint8_t *data;
+} tcp_packet_t;
 
 #endif
