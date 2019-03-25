56b8d24b7e524c15c7a91af65c83e562f543930d
RMuskovets <rmuskovets@gmail.com>
2019-03-25 09:40:30 +0200

    Add TCP functions (send packet, handle packet)

--- src/include/tcp.h
+++ src/include/tcp.h
@@ -15,4 +15,10 @@ typedef struct tcp_packet {
 	uint8_t *data;
 } tcp_packet_t;
 
+void tcp_send_packet(uint8_t * dst_ip, uint16_t src_port, uint16_t dst_port, void * data, int len);
+void tcp_send_packet(tcp_packet_t * packet);
+
+void tcp_handle_packet(tcp_packet_t * packet);
+
+
 #endif
